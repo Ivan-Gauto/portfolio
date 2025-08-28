75c5b4f1d006a607a6fe9c12a2af3f3695f6b468
Ivan-Gauto <ivangauto602160@gmail.com>
2025-08-28 13:13:01 -0300

    Se arreglo vista de experiencia

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -21,24 +21,20 @@ export default function Experience() {
     <div className="text-white container py-5">
       <h1 data-aos="fade-right" data-aos-delay="400" className="text-center mb-5">Experiencia</h1>
 
-      <div data-aos="fade-down" data-aos-delay="200" className="timeline position-relative border-start border-2 border-light">
+      <div className="timeline position-relative border-start border-2 border-light">
         {experiences.map((exp, i) => (
           <div key={i} className="mb-5 ms-4">
             {/* PUNTO */}
-            <div
-              className="position-absolute bg-light"
-              style={{
-                width: "15px",
-                height: "15px",
-                left: "-8px",
-                marginTop: "11.7px"
-              }}
+            <div data-aos="zoom-in" data-aos-delay="100"
+              className="position-absolute bg-light punto-timeline"
             ></div>
 
             {/* CONTENIDO */}
-            <h2 className="fw-light mb-4">{exp.year}</h2>
-            <h3 style={{color:"lime"}} className="m-0">{exp.title}</h3>
-            <p className="text-secondary m-0">{exp.description}</p>
+            <div data-aos="zoom-in" data-aos-delay="300">
+              <h2 className="fw-light mb-3">{exp.year}</h2>
+              <h3 style={{ color: "lime" }} className="m-0">{exp.title}</h3>
+              <p className="text-secondary m-0">{exp.description}</p>
+            </div>
           </div>
         ))}
       </div>
